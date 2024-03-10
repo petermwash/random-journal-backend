2ff2bdf5c53c06f96ad4a0f209b9c05a168d7e0f
petermwash <petermwash48@gmail.com>
2024-03-10 16:01:46 +0300

    feat(emails) send random emails
    - Implement the functionality to send emails
    - Configure cron-job to run everyday at 12pm EAT and trigger send emails
    
    [Delivers #]

--- src/db/journals.ts
+++ src/db/journals.ts
@@ -3,9 +3,18 @@ import mongoose from "mongoose";
 const JournalSchema = new mongoose.Schema({
     content: { type: String, required: true },
     author: { type: String, required: true },
-})
+  },
+  {
+    timestamps: true,
+  });
 
 export const JournalModel = mongoose.model("Journal", JournalSchema);
 
 export const createJournal = (values: Record<string, any>) => new JournalModel(values).save().then((journal) => journal.toObject());
 export const getJournals = () => JournalModel.find();
+export const getLastDayJournals = () => {
+  const twentyFourHoursAgo = new Date();
+  twentyFourHoursAgo.setDate(twentyFourHoursAgo.getDate() - 1);
+
+  return JournalModel.find({ createdAt: { $gte: twentyFourHoursAgo } });
+}
--- src/db/users.ts
+++ src/db/users.ts
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
  const UserSchema = new mongoose.Schema({
     fUserId: { type: String, required: true },
     email: { type: String, required: true },
- });
+  },
+  {
+    timestamps: true,
+  });
 
  export const UserModel = mongoose.model("User", UserSchema);
 
  export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
  export const getUserByEmail = (email: string) => UserModel.findOne({ email });
- export const getUsers = () => UserModel.find();
- export const getUserById = (id: string) => UserModel.findById(id);
+ export const getUserEmailsByUid = (userIds: string[]) => UserModel.find({ _id: { $in: userIds } });
